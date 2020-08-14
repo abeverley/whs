2e6b5e9ab67e5c2f944f7df6d1729cd8ce521017
Andy Beverley <a.beverley@ctrlo.com>
2020-08-14 12:55:19 +0100

    Add checks to signup form

--- js/app.js
+++ js/app.js
@@ -1,25 +1,35 @@
 $("#join-form")
 .on("submit", function (ev, frm) {
-  $('#form-submit').html('Submitting...');
-  $.ajax({
-    url: "/cgi-bin/join.cgi",
-    method: "POST",
-    data: $(ev.target).serialize(),
-    dataType: "json",
-    success: function(data) {
-      $(ev.target)[0].reset();
-      $("#thanks").show();
-      $('#form-submit').hide();
-    },
-    error: function(xhr, textStatus, error){
-      $('#form-error').html("Form submission failed: " + error);
-      $('#form-error').show();
-      $('#form-submit').html('Submit');
-      console.log(xhr.statusText);
-      console.log(textStatus);
-      console.log(error);
-    }
-  });
+  if (!$('#forename').val().length) {
+    alert("Please enter your first name");
+  } else if (!$('#surname').val().length) {
+    alert("Please enter your surname");
+  } else if (!$('#email').val().length) {
+    alert("Please enter your email address");
+  } else if (!$('#permission').prop('checked')) {
+    alert("Please select the checkbox giving us permission to contact you");
+  } else {
+    $('#form-submit').html('Submitting...');
+    $.ajax({
+      url: "/cgi-bin/join.cgi",
+      method: "POST",
+      data: $(ev.target).serialize(),
+      dataType: "json",
+      success: function(data) {
+        $(ev.target)[0].reset();
+        $("#thanks").show();
+        $('#form-submit').hide();
+      },
+      error: function(xhr, textStatus, error){
+        $('#form-error').html("Form submission failed: " + error);
+        $('#form-error').show();
+        $('#form-submit').html('Submit');
+        console.log(xhr.statusText);
+        console.log(textStatus);
+        console.log(error);
+      }
+    });
+  }
 })
 // to prevent form from submitting upon successful validation
 .on("submit", function(ev) {
